61040242ea56bb60322555230353b1843733a337
rindoshingen <zenjukai@gmail.com>
2024-10-05 07:14:33 -0600

    Refactored controls and template tags

--- app.py
+++ app.py
@@ -6,4 +6,24 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/soto-zen')
+def zen():
+	return render_template('soto-zen.html')
+
+@app.route('/coding')
+def coding():
+	return render_template('coding.html')
+
+@app.route('/bookshelf')
+def books():
+	return render_template('bookshelf.html')
+
+@app.route('/resources')
+def resources():
+	return render_template('resources.html')
+
+@app.route('/colophon')
+def colophon():
+	return render_template('colophon.html')
+
 app.run(debug=True)
